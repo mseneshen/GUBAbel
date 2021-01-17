886676c73a3a0ea12c5fe81cbec998bbb6ae21a8
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2021-01-16 19:45:14 -0700

    forgot to add the captions.vue file lmao 😂😂😂😂😂

---
+++ src/views/Captions.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <p class="caption">{{ liveCaption }}</p>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Captions",
+  data: () => ({
+    liveCaption: "haha text goes brrrrrr"
+  })
+});
+</script>
